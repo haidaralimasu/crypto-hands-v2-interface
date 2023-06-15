f2b6c73a9628deb36a436adb9c2e0d353a49e355
haidaralimasu <haidaralimasu123@gmail.com>
2023-06-15 12:42:39 +0530

    updated graph

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ export const infuraId = "d014af161a4b4ffbaa358366e232e2c8";
 export const cryptoHandsAddress = "0x254d1Ca148C37E30B1409f7487Eb77E6a2cF6Ab5";
 export const rpsAddress = "0x07A8560053B0e057a9bb6C78cE2a3D037f5baa8a";
 export const graphEndpoint =
-  "https://api.thegraph.com/subgraphs/name/haidaralimasu/crypto-hands-subgraph";
+  "https://api.thegraph.com/subgraphs/name/haidaralimasu/crypto-hands-v2";
 
 export const prices = [
   {
